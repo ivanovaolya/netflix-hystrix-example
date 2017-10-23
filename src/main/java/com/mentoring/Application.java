ca08c2be82d8d942c46afcfe52f2eac6ac469584
Olha Melnyk <Olha_Melnyk@epam.com>
2017-10-23 15:05:36 +0300

    Commented spring-cloud-hystrix

--- src/main/java/com/mentoring/Application.java
+++ src/main/java/com/mentoring/Application.java
@@ -2,16 +2,14 @@ package com.mentoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @author ivanovaolyaa
  * @version 10/18/2017
  */
 @SpringBootApplication
-@EnableCircuitBreaker
-@EnableHystrixDashboard
+//@EnableCircuitBreaker
+//@EnableHystrixDashboard
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/mentoring/service/GreetingService.java
+++ src/main/java/com/mentoring/service/GreetingService.java
@@ -1,7 +1,5 @@
 package com.mentoring.service;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GreetingService {
 
-    @HystrixCommand(fallbackMethod = "defaultGreeting")
+    //@HystrixCommand(fallbackMethod = "defaultGreeting")
     public String getGreeting(String username) {
         return new RestTemplate()
                 .getForObject("http://localhost:8086/greeting/{username}",
